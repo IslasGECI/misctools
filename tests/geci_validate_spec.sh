c6780fca1e4e9d82e16fa2c175f26c1b6298e3e4
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-03-07 18:16:51 -0800

    🚚 Move sample TDP to a subdirectory

--- tests/geci_validate_spec.sh
+++ tests/geci_validate_spec.sh
@@ -4,22 +4,22 @@
 Describe "geci-validate"
 
   It "requires directory path as first argument"
-    cd tests
-    When call ../src/geci-validate
+    cd tests/valid_tdp
+    When call ../../src/geci-validate
     The status should be failure
     The stderr should include "'datapackage.json' does not exist."
     The stdout should include "# invalid: datapackage.json"
   End
 
   It "accepts a relative path as first argument"
-    When call src/geci-validate tests
+    When call src/geci-validate tests/valid_tdp
     The status should be success
     The stdout should include "# valid: morfometria_albatros-laysan_guadalupe.csv"
   End
 
   It "accepts a dot as first argument"
-    cd tests
-    When call ../src/geci-validate .
+    cd tests/valid_tdp
+    When call ../../src/geci-validate .
     The status should be success
     The stdout should include "# valid: morfometria_albatros-laysan_guadalupe.csv"
   End
