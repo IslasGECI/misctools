cb9d87807ff7e81f089b3e5d530b78b71fe6fdfc
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-02-23 11:49:41 -0800

    ✅ Pass tests for check-jsonschema

--- tests/checkanalyses_spec.sh
+++ tests/checkanalyses_spec.sh
@@ -6,12 +6,13 @@ Describe "geci-checkanalyses"
   It "requires image_tag"
     When call geci-checkanalyses tests/checkanalyses/analyses_without_imagetag.json
     The status should be failure
-    The stderr should include "'image_tag' is a required property"
+    The stdout should include "'image_tag' is a required property"
   End
 
   It "accepts image_tag"
     When call geci-checkanalyses tests/checkanalyses/analyses.json
     The status should be success
+    The stdout should include "ok -- validation done"
   End
 
 End
